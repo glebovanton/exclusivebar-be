6314ecc87e0e5b27b331bf6059ee0b21a96207b1
Anton <workglebov@gmail.com>
2021-08-30 14:56:23 +0300

    feat: Refactor schema

--- product-service/src/functions/getAllProducts/handler.ts
+++ product-service/src/functions/getAllProducts/handler.ts
@@ -7,75 +7,64 @@ import { middyfy } from "@libs/lambda";
 import schema from "./schema";
 
 const getAll: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  return formatJSONResponse({
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Methods": "*",
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
+  return formatJSONResponse([
+    {
+      count: 4,
+      description: "[AWG] Short Product Description1",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+      price: 2.4,
+      title: "[AWG] ProductOne",
     },
-    body: JSON.stringify({
-      products: [
-        {
-          count: 4,
-          description: "[AWG] Short Product Description1",
-          id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-          price: 2.4,
-          title: "[AWG] ProductOne",
-        },
-        {
-          count: 6,
-          description: "[AWG]Short Product Description3",
-          id: "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
-          price: 10,
-          title: "[AWG] ProductNew",
-        },
-        {
-          count: 7,
-          description: "[AWG] Short Product Description2",
-          id: "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
-          price: 23,
-          title: "[AWG] ProductTop",
-        },
-        {
-          count: 12,
-          description: "[AWG] Short Product Description7",
-          id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
-          price: 15,
-          title: "[AWG] ProductTitle",
-        },
-        {
-          count: 7,
-          description: "[AWG] Short Product Description2",
-          id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-          price: 23,
-          title: "[AWG] Product",
-        },
-        {
-          count: 8,
-          description: "[AWG] Short Product Description4",
-          id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-          price: 15,
-          title: "[AWG] ProductTest",
-        },
-        {
-          count: 2,
-          description: "[AWG] Short Product Descriptio1",
-          id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-          price: 23,
-          title: "[AWG] Product2",
-        },
-        {
-          count: 3,
-          description: "[AWG] Short Product Description7",
-          id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-          price: 15,
-          title: "[AWG] ProductName",
-        },
-      ],
-    }),
-  });
+    {
+      count: 6,
+      description: "[AWG]Short Product Description3",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
+      price: 10,
+      title: "[AWG] ProductNew",
+    },
+    {
+      count: 7,
+      description: "[AWG] Short Product Description2",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
+      price: 23,
+      title: "[AWG] ProductTop",
+    },
+    {
+      count: 12,
+      description: "[AWG] Short Product Description7",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+      price: 15,
+      title: "[AWG] ProductTitle",
+    },
+    {
+      count: 7,
+      description: "[AWG] Short Product Description2",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+      price: 23,
+      title: "[AWG] Product",
+    },
+    {
+      count: 8,
+      description: "[AWG] Short Product Description4",
+      id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+      price: 15,
+      title: "[AWG] ProductTest",
+    },
+    {
+      count: 2,
+      description: "[AWG] Short Product Descriptio1",
+      id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+      price: 23,
+      title: "[AWG] Product2",
+    },
+    {
+      count: 3,
+      description: "[AWG] Short Product Description7",
+      id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+      price: 15,
+      title: "[AWG] ProductName",
+    },
+  ]);
 };
 
 export const main = middyfy(getAll);
--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,5 +1,5 @@
 import "source-map-support/register";
-
+import { StatusCodes } from "http-status-codes";
 import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
 import { formatJSONResponse } from "@libs/apiGateway";
 import { middyfy } from "@libs/lambda";
@@ -9,24 +9,25 @@ import schema from "./schema";
 const getById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
-  return formatJSONResponse({
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Methods": "*",
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      product: {
+  const { id } = event.pathParameters;
+  return +id < 10
+    ? formatJSONResponse({
         count: 4,
-        description: `Product id-${event.pathParameters.id}. Short Product Description`,
+        description: `Product id-${id}. Short Product Description`,
         id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
         price: 2.4,
         title: "[MOCK] ProductOne",
-      },
-    }),
-  });
+      })
+    : {
+        statusCode: StatusCodes.NOT_FOUND,
+        body: "Product not found",
+        headers: {
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      };
 };
 
 export const main = middyfy(getById);
