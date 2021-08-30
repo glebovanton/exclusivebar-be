f5ae3106565d492e43c3b9a3b0aadaca5bfc2f68
Anton <workglebov@gmail.com>
2021-08-30 14:57:42 +0300

    feat: Refactor cors

--- product-service/src/libs/apiGateway.ts
+++ product-service/src/libs/apiGateway.ts
@@ -1,12 +1,28 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import { StatusCodes } from "http-status-codes";
+import type {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from "aws-lambda";
 import type { FromSchema } from "json-schema-to-ts";
 
-type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, "body"> & {
+  body: FromSchema<S>;
+};
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
+  ValidatedAPIGatewayProxyEvent<S>,
+  APIGatewayProxyResult
+>;
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatJSONResponse = (response) => {
   return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
-}
+    statusCode: StatusCodes.OK,
+    body: JSON.stringify(response),
+    headers: {
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  };
+};
