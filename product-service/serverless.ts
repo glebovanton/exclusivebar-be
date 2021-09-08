9d6db49bbce8570bd4b0838c6788b60f1862c63c
Anton <workglebov@gmail.com>
2021-09-09 00:26:29 +0300

    feat: Refactoring

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -29,6 +29,11 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       WEATHERSTACK_API_KEY: "${env:WEATHERSTACK_API_KEY}",
+      PG_HOST: "${env:PG_HOST}",
+      PG_PORT: "${env:PG_PORT}",
+      PG_DATABASE: "${env:PG_DATABASE}",
+      PG_USERNAME: "${env:PG_USERNAME}",
+      PG_PASSWORD: "${env:PG_PASSWORD}",
     },
     lambdaHashingVersion: "20201221",
   },
