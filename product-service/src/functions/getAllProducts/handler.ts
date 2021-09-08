d20a6088259c46b1dd76539d8ac366432db728e2
Anton <workglebov@gmail.com>
2021-09-08 23:39:56 +0300

    feat: Fix getProducts

--- product-service/src/functions/getAllProducts/handler.ts
+++ product-service/src/functions/getAllProducts/handler.ts
@@ -1,11 +1,12 @@
 import "source-map-support/register";
 import { formatJSONResponse } from "../../libs/apiGateway";
 import { middyfy } from "../../libs/lambda";
-import { products } from "./mocks";
+import { getProducts } from "../../helpers/getProducts";
 
 import { HttpResponse } from "@functions/getWeather/handler";
 
 export async function getAll(): HttpResponse {
+  const products = await getProducts();
   return formatJSONResponse(products);
 }
 
