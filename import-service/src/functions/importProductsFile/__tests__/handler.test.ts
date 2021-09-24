19a37bb5e0f11a443c1766a300b4b0d9b25aace7
Anton <workglebov@gmail.com>
2021-09-24 17:54:44 +0300

    feat: Add importProductsFile testCase

--- import-service/src/functions/importProductsFile/__tests__/handler.test.ts
+++ import-service/src/functions/importProductsFile/__tests__/handler.test.ts
@@ -5,9 +5,19 @@ interface Result {
   statusCode?: number;
   body?: string;
 }
-const testName = 'example.csv'
+const testName = "example.csv";
 
 describe("ImportProductsFile lambda function", () => {
+  test("should return status code 200", async () => {
+    const event = {
+      queryStringParameters: {
+        name: testName,
+      },
+    } as any;
+    const result: Result = await importProductsFile(event);
+    expect(result.statusCode).toEqual(200);
+  });
+
   test("should return signed url", async () => {
     const event = {
       queryStringParameters: {
